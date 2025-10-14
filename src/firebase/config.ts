62377dad0cce0a482aaca9c4414cc663aea30396
Nimsara Bro <nimsarabro81@gmail.com>
2025-10-14 12:19:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2082724637-399ae",
+  "appId": "1:600697618793:web:a7c6856136b6ad63fbbdf8",
+  "apiKey": "AIzaSyDvWipp4EUmuO4-fuMn9QdqameThrcx3OI",
+  "authDomain": "studio-2082724637-399ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "600697618793"
+};
